0e7d2de9144189b226021ede3d086008ddbb4980
John Gallaugher <john.gallaugher@bc.edu>
2018-07-31 21:22:37 -0400

    9.3 complete - String Manipulation with Components Method

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -14,13 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
-    
+
     var formulaArray = ["miles to kilometers",
                         "kilometers to miles",
                         "feet to meters",
                         "yards to meters",
                         "meters to feet",
                         "meters to yards"]
+    var fromUnits = ""
+    var toUnits = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,7 +49,11 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        fromUnitsLabel.text = formulaArray[row]
+        let unitsArray = formulaArray[row].components(separatedBy: " to ")
+        fromUnits = unitsArray[0]
+        toUnits = unitsArray[1]
+        fromUnitsLabel.text = fromUnits
+        resultsLabel.text = toUnits
     }
 }
 
